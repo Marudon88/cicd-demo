1578fad81a41cae620af9ba5ce2ccb4a3cbba8ca
Steven <steven.albrecht@docc.techstarter.de>
2023-11-21 14:30:48 +0100

    Git Workflow IX

--- infra/modules/s3-website/main.tf
+++ infra/modules/s3-website/main.tf
@@ -29,3 +29,21 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          "${aws_s3_bucket.this.arn}/*",
+          "${aws_s3_bucket.this.arn}",
+        ]
+      },
+    ]
+  })
+}
